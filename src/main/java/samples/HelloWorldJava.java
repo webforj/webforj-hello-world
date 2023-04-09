b7fc0d990dd1869962271d46fe476afbc0705243
Stephan Wald <swald@basis.com>
2023-04-09 10:27:45 +0200

    0.16.0 cosmetics, include dwcj-install-maven-plugin

--- src/main/java/samples/HelloWorldJava.java
+++ src/main/java/samples/HelloWorldJava.java
@@ -8,30 +8,34 @@ import org.dwcj.component.label.Label;
 import org.dwcj.component.window.Frame;
 import org.dwcj.exceptions.DwcjAppInitializeException;
 
+/**
+ * A simple HelloWorld class.
+ */
 public class HelloWorldJava extends App {
-    @Override
-    public void run() throws DwcjAppInitializeException {
 
-        Frame panel = new Frame();
-        panel.setStyle("display", "inline-grid");
-        panel.setStyle("gap", "20px");
-        panel.setStyle("margin", "20px");
-        panel.setStyle("padding", "20px");
-        panel.setStyle("border", "1px dotted");
+  @Override
+  public void run() throws DwcjAppInitializeException {
 
-        panel.add(new Label("Hello World"));
+    Frame panel = new Frame();
+    panel.setStyle("display", "inline-grid");
+    panel.setStyle("gap", "20px");
+    panel.setStyle("margin", "20px");
+    panel.setStyle("padding", "20px");
+    panel.setStyle("border", "1px dotted");
 
-        Button btn = new Button("Say Hello");
-        panel.add(btn);
+    panel.add(new Label("Hello World"));
 
-        btn.setTheme(Button.Theme.SUCCESS);
-        btn.setExpanse(Button.Expanse.XLARGE);
+    Button btn = new Button("Say Hello");
+    panel.add(btn);
 
-        btn.onClick(this::onButtonPush);
-    }
+    btn.setTheme(Button.Theme.SUCCESS);
+    btn.setExpanse(Button.Expanse.XLARGE);
 
-    private void onButtonPush(ButtonClickEvent buttonClickEvent) {
-        App.msgbox("Hello DWCJ!");
-    }
+    btn.onClick(this::onButtonPush);
+  }
+
+  private void onButtonPush(ButtonClickEvent buttonClickEvent) {
+    App.msgbox("Hello DWCJ!");
+  }
 
 }
