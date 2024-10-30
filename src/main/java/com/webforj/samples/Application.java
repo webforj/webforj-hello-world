f120d1f07b11e7292025cb1ca5a21476fd9bc7b6
Hyyan Abo Fakher <hyyanaf@gmail.com>
2024-10-30 11:35:20 +0100

    cleanup

--- src/main/java/com/webforj/samples/Application.java
+++ src/main/java/com/webforj/samples/Application.java
@@ -1,20 +1,15 @@
-package samples;
+package com.webforj.samples;
 
 import com.webforj.App;
 import com.webforj.annotation.AppTitle;
 import com.webforj.annotation.InlineStyleSheet;
 import com.webforj.annotation.Routify;
-import com.webforj.exceptions.WebforjException;
 
 /**
  * A simple HelloWorld app.
  */
-@Routify(packages = "samples.views")
+@Routify(packages = "com.webforj.samples.views")
 @AppTitle("webforJ Hello World")
 @InlineStyleSheet("context://styles/styles.css")
 public class Application extends App{
-  
-  @Override
-  public void run() throws WebforjException {
-  }
 }
--- src/main/java/com/webforj/samples/views/HelloWorldView.java
+++ src/main/java/com/webforj/samples/views/HelloWorldView.java
@@ -1,4 +1,4 @@
-package samples.views;
+package com.webforj.samples.views;
 
 import static com.webforj.component.optiondialog.OptionDialog.showMessageDialog;
 
@@ -10,17 +10,17 @@ import com.webforj.component.html.elements.Paragraph;
 import com.webforj.router.annotation.Route;
 
 /**
- * A simple HelloWorld app.
+ * A simple HelloWorld view.
  */
 @Route("/")
-public class WebforjHelloWorldView extends Composite<Div> {
+public class HelloWorldView extends Composite<Div> {
   
   private Div self = getBoundComponent();
   private Paragraph hello = new Paragraph("Hello World!");
   private Button btn = new Button("Say Hello");
 
-  public WebforjHelloWorldView(){
-    self.addClassName("mainFrame");
+  public HelloWorldView(){
+    self.addClassName("hello-world-view");
 
     btn.setTheme(ButtonTheme.PRIMARY)
         .addClickListener(e -> showMessageDialog("Hello World!", "webforJ Message"));
